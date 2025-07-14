fb936ec8bd550e9f7675af7affd01fe3b059d3d6
Keerrthivarman <Keerthivarmans2004@gmail.com>
2025-07-14 13:26:42 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,188 @@
+/*//wap to print the prime no in the array
+#include<stdio.h>
+int main(){
+int n,i,c=0,j;
+scanf("%d",&n);
+int arr[n];
+for(i=0;i<n;i++)
+scanf("%d",&arr[i]);
+for(int i=0;i<n;i++){
+for(j=2;j<arr[i];j++){
+if(arr[i]%j==0)
+break;
+}
+if(arr[i]==j){
+printf("%d ",arr[i]);
+c++;
+}
+}
+printf("\nc=%d",c);
+}
+
+//wap to scan the elements of the array only if the elements are prime
+#include<stdio.h>
+int main(){
+int n,i,j;
+scanf("%d",&n);
+int arr[n];
+for(i=0;i<n;){
+scanf("%d",&arr[i]);
+for(j=2;j<arr[i];j++){
+if(arr[i]%j==0)
+break;
+}
+if(arr[i]==j)
+i++;
+}
+
+for(i=0;i<n;i++)
+printf("%d ",arr[i]);
+}
+//wap to  reverse the element in the array
+#include<stdio.h>
+int main(){
+int n,i,j,temp;
+scanf("%d",&n);
+int arr[n];
+for(i=0;i<n;i++)
+scanf("%d",&arr[i]);
+printf("before: ");
+for(i=0;i<n;i++)
+printf("%d ",arr[i]);
+for(i=0,j=n-1;i<j;i++,j--){
+temp=arr[i];
+arr[i]=arr[j];
+arr[j]=temp;
+}
+printf("\nafter: ");
+for(i=0;i<n;i++)
+printf("%d ",arr[i]);
+}
+//wap to reverse the first half and the second half of the given array
+#include<stdio.h>
+int main(){
+int n,i,j,temp;
+scanf("%d",&n);
+int arr[n];
+for(i=0;i<n;i++)
+scanf("%d",&arr[i]);
+printf("before: ");
+for(i=0;i<n;i++)
+printf("%d ",arr[i]);
+for(i=0,j=n/2;j<n;i++,j++){
+temp=arr[i];
+arr[i]=arr[j];
+arr[j]=temp;
+}
+printf("\nafter: ");
+for(i=0;i<n;i++)
+printf("%d ",arr[i]); 
+
+}*/
+//wap to swap the adjecent element in the array
+#include<stdio.h>
+int main(){
+int n,i,j,temp;
+scanf("%d",&n);
+int arr[n];
+for(i=0;i<n;i++)
+scanf("%d",&arr[i]);
+printf("before: ");
+for(i=0;i<n;i++)
+printf("%d ",arr[i]);
+for(i=0,j=1;i<n;i+=2,j+=2){
+temp=arr[i];
+arr[i]=arr[j];
+arr[j]=temp;
+}
+printf("\nafter: ");
+for(i=0;i<n;i++)
+printf("%d ",arr[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
